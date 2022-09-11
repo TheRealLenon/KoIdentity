a0056e117a990f5745a30be7a49f88426e0c62c7
TheRealLenon <thereallenon@icloud.com>
2022-09-11 05:50:03 +0200

    Updating tests for the entity store.

---
+++ test/Abstraction.Test/Migrations/20220911034814_MakingHelperPropUnique.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tekoding.KoIdentity.Abstraction.Test.Migrations
+{
+    public partial class MakingHelperPropUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestProp",
+                schema: "KoIdentity.Abstraction.Tests",
+                table: "BaseEntity",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseEntity_TestProp",
+                schema: "KoIdentity.Abstraction.Tests",
+                table: "BaseEntity",
+                column: "TestProp",
+                unique: true,
+                filter: "[TestProp] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BaseEntity_TestProp",
+                schema: "KoIdentity.Abstraction.Tests",
+                table: "BaseEntity");
+
+            migrationBuilder.DropColumn(
+                name: "TestProp",
+                schema: "KoIdentity.Abstraction.Tests",
+                table: "BaseEntity");
+        }
+    }
+}
--- test/Abstraction.Test/Stores/EntityStoreTests.cs
+++ test/Abstraction.Test/Stores/EntityStoreTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +16,7 @@ namespace Tekoding.KoIdentity.Abstraction.Test.Stores;
 public class EntityStoreTests
 {
 #nullable disable
+    private readonly BaseEntityValidator _baseEntityValidator = new();
     private DbContextOptions _dbContextOptions;
 #nullable restore
 
@@ -59,71 +61,265 @@ public class EntityStoreTests
     public async Task CreateAsync_Succeeds()
     {
         await using var ctx = new DatabaseContext(_dbContextOptions);
-        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, new BaseEntityValidator());
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
 
-        var entityToCreate = new BaseEntity();
+        var entity = new BaseEntity();
 
         var entityCountBeforeCreation = await ctx.Set<BaseEntity>().CountAsync();
-        var dateBeforeCreation = DateTime.UtcNow;
-        var creationResult = await entityStore.CreateAsync(entityToCreate);
+        var creationResult = await entityStore.CreateAsync(entity);
         var entityCountAfterCreation = await ctx.Set<BaseEntity>().CountAsync();
 
         creationResult.State.Should().BeTrue();
         creationResult.Payload.Should().BeNull();
         creationResult.ErrorCount.Should().Be(0);
         creationResult.ToString().Should().Contain("Succeeded");
-        entityToCreate.Id.Should().NotBe(Guid.Empty);
-        entityToCreate.ChangeDate.Should().Be(DateTime.MinValue);
-        entityToCreate.CreationDate.Should().BeOnOrAfter(dateBeforeCreation);
         entityCountAfterCreation.Should().Be(entityCountBeforeCreation + 1);
     }
 
     /// <summary>
-    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate an failed operation with the error
-    /// <b>ObjectNullFailure</b>, if the entity was not created because the validation failed.
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a failed operation, if the entity was not created
+    /// due to an <b>DatabaseCreationFailure</b>.
     /// </summary>
     [Test]
-    public async Task CreateAsync_ValidateDatabaseObjectNullFailure()
+    public async Task CreateAsync_FailsWithDatabaseCreationFailure()
     {
         await using var ctx = new DatabaseContext(_dbContextOptions);
-        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, new BaseEntityValidator());
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
 
-        BaseEntity? entityToCreate = null;
+        var entity = new BaseEntity();
+        await entityStore.CreateAsync(entity);
 
         var entityCountBeforeCreation = await ctx.Set<BaseEntity>().CountAsync();
-#pragma warning disable CS8604
-        var creationResult = await entityStore.CreateAsync(entityToCreate);
-#pragma warning restore CS8604
+        var creationResult = await entityStore.CreateAsync(entity);
         var entityCountAfterCreation = await ctx.Set<BaseEntity>().CountAsync();
 
         creationResult.State.Should().BeFalse();
-        creationResult.Payload.Should().NotBeNull().And.BeOfType<Error[]>();
         creationResult.ErrorCount.Should().Be(1);
-        creationResult.ToString().Should().Contain("ObjectNullFailure");
+        creationResult.Payload.Should().NotBeNull().And.BeOfType<Error[]>();
+        creationResult.ToString().Should()
+            .ContainAll(nameof(ErrorDescriber.DatabaseCreationFailure));
         entityCountAfterCreation.Should().Be(entityCountBeforeCreation);
     }
 
     /// <summary>
-    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate an failed operation with the error
-    /// <b>DatabaseCreationFailure</b>, if the entity was not created because the could not be created in the database.
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a successful operation, if the entity was
+    /// successfully updated in the database.
     /// </summary>
     [Test]
-    public async Task CreateAsync_ValidateDatabaseCreationFailure()
+    public async Task UpdateAsync_Succeeds()
     {
         await using var ctx = new DatabaseContext(_dbContextOptions);
-        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, new BaseEntityValidator());
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
 
-        var entityToCreate = new BaseEntity();
-        await entityStore.CreateAsync(entityToCreate);
+        var entity = new BaseEntity();
+        await entityStore.CreateAsync(entity);
+        await Task.Delay(100);
 
-        var entityCountBeforeCreation = await ctx.Set<BaseEntity>().CountAsync();
-        var creationResult = await entityStore.CreateAsync(entityToCreate);
-        var entityCountAfterCreation = await ctx.Set<BaseEntity>().CountAsync();
+        entity.TestProp = "New Updated Value";
 
-        creationResult.State.Should().BeFalse();
-        creationResult.Payload.Should().NotBeNull().And.BeOfType<Error[]>();
-        creationResult.ErrorCount.Should().Be(1);
-        creationResult.ToString().Should().Contain("DatabaseCreationFailure");
-        entityCountAfterCreation.Should().Be(entityCountBeforeCreation);
+        var updateResult = await entityStore.UpdateAsync(entity);
+
+        updateResult.State.Should().BeTrue();
+        updateResult.Payload.Should().BeNull();
+        updateResult.ErrorCount.Should().Be(0);
+        updateResult.ToString().Should().Contain("Succeeded");
+        entity.ChangeDate.Should().BeAfter(entity.CreationDate);
+    }
+
+    /// <summary>
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a failed operation, if the entity was not updated
+    /// due to an <b>DatabaseUpdateFailure</b>.
+    /// </summary>
+    [Test]
+    public async Task UpdateAsync_FailsWithDatabaseUpdateFailure()
+    {
+        await using var ctx = new DatabaseContext(_dbContextOptions);
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
+
+        var firstEntityToCreate = new BaseEntity { TestProp = "Unique" };
+        var secondEntityToCreate = new BaseEntity();
+        await entityStore.CreateAsync(firstEntityToCreate);
+        await entityStore.CreateAsync(secondEntityToCreate);
+
+        secondEntityToCreate.TestProp = "Unique";
+
+        var updateResult = await entityStore.UpdateAsync(secondEntityToCreate);
+
+        updateResult.State.Should().BeFalse();
+        updateResult.ErrorCount.Should().Be(1);
+        updateResult.Payload.Should().NotBeNull().And.BeOfType<Error[]>();
+        updateResult.ToString().Should()
+            .ContainAll(nameof(ErrorDescriber.DatabaseUpdateFailure));
+    }
+
+    /// <summary>
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a failed operation, if the entity was not updated
+    /// due to an <b>ObjectStateUnmodifiedFailure</b>.
+    /// </summary>
+    [Test]
+    public async Task UpdateAsync_FailsWithObjectStateUnmodifiedFailure()
+    {
+        await using var ctx = new DatabaseContext(_dbContextOptions);
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
+
+        var entity = new BaseEntity();
+
+        var updateResult = await entityStore.UpdateAsync(entity);
+
+        updateResult.State.Should().BeFalse();
+        updateResult.ErrorCount.Should().Be(1);
+        updateResult.Payload.Should().NotBeNull().And.BeOfType<Error[]>();
+        updateResult.ToString().Should()
+            .ContainAll(nameof(ErrorDescriber.ObjectStateUnmodifiedFailure));
+    }
+
+    /// <summary>
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a successful operation, if the entity was
+    /// successfully deleted from the database.
+    /// </summary>
+    [Test]
+    public async Task DeleteAsync_Succeeds()
+    {
+        await using var ctx = new DatabaseContext(_dbContextOptions);
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
+
+        var entity = new BaseEntity();
+        await entityStore.CreateAsync(entity);
+
+        var entityCountBeforeDeletion = await ctx.Set<BaseEntity>().CountAsync();
+        var deletionResult = await entityStore.DeleteAsync(entity);
+        var entityCountAfterDeletion = await ctx.Set<BaseEntity>().CountAsync();
+
+        deletionResult.State.Should().BeTrue();
+        deletionResult.Payload.Should().BeNull();
+        deletionResult.ErrorCount.Should().Be(0);
+        deletionResult.ToString().Should().Contain("Succeeded");
+        entityCountAfterDeletion.Should().Be(entityCountBeforeDeletion - 1);
+    }
+
+    /// <summary>
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a failed operation, if the entity was not updated
+    /// due to an <b>DatabaseDeletionFailure</b>.
+    /// </summary>
+    [Test]
+    public async Task DeleteAsync_FailsWithDatabaseDeletionFailure()
+    {
+        await using var ctx = new DatabaseContext(_dbContextOptions);
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
+
+        var entity = new BaseEntity();
+
+        var deletionResult = await entityStore.DeleteAsync(entity);
+
+        deletionResult.State.Should().BeFalse();
+        deletionResult.ErrorCount.Should().Be(1);
+        deletionResult.Payload.Should().NotBeNull().And.BeOfType<Error[]>();
+        deletionResult.ToString().Should()
+            .ContainAll(nameof(ErrorDescriber.DatabaseDeletionFailure));
+    }
+
+    /// <summary>
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a failed operation, if the entity was not updated
+    /// due to an <b>DatabaseSelectionFailure</b>.
+    /// </summary>
+    [Test]
+    public async Task GetAllAsync_FailsWithDatabaseSelectionFailure()
+    {
+        await using var ctx = new DatabaseContext(_dbContextOptions);
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
+
+        await ctx.DisposeAsync();
+
+        var selectionResult = await entityStore.GetAllAsync();
+
+        selectionResult.State.Should().BeFalse();
+        selectionResult.ErrorCount.Should().Be(1);
+        selectionResult.Payload.Should().NotBeNull().And.BeOfType<Error[]>();
+        selectionResult.ToString().Should()
+            .ContainAll(nameof(ErrorDescriber.DatabaseSelectionFailure));
+    }
+
+    /// <summary>
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a successful operation, if all entities were
+    /// successfully loaded from the database.
+    /// </summary>
+    [Test]
+    public async Task GetAllAsync_Succeeds()
+    {
+        await using var ctx = new DatabaseContext(_dbContextOptions);
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
+
+        var entity = new BaseEntity();
+        await entityStore.CreateAsync(entity);
+
+        var selectionResult = await entityStore.GetAllAsync();
+
+        selectionResult.State.Should().BeTrue();
+        selectionResult.ErrorCount.Should().Be(0);
+        selectionResult.Payload.Should().NotBeNull().And.BeOfType<List<BaseEntity>>();
+        selectionResult.Payload.As<List<BaseEntity>>().Count.Should().Be(await ctx.Set<BaseEntity>().CountAsync());
+        selectionResult.ToString().Should().Contain("Succeeded");
+    }
+
+    /// <summary>
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a failed operation, if the entity was not updated
+    /// due to an <b>DatabaseSelectionFailure</b>.
+    /// </summary>
+    [Test]
+    public async Task FindByIdAsync_FailsWithDatabaseSelectionFailure()
+    {
+        await using var ctx = new DatabaseContext(_dbContextOptions);
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
+
+        await ctx.DisposeAsync();
+
+        var findByIdResult = await entityStore.FindByIdAsync(Guid.NewGuid());
+
+        findByIdResult.State.Should().BeFalse();
+        findByIdResult.ErrorCount.Should().Be(1);
+        findByIdResult.Payload.Should().NotBeNull().And.BeOfType<Error[]>();
+        findByIdResult.ToString().Should()
+            .ContainAll(nameof(ErrorDescriber.DatabaseSelectionFailure));
+    }
+
+    /// <summary>
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a successful operation, if a single entity was
+    /// successfully loaded from the database.
+    /// </summary>
+    [Test]
+    public async Task FindByIdAsync_Succeeds()
+    {
+        await using var ctx = new DatabaseContext(_dbContextOptions);
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
+
+        var entity = new BaseEntity();
+        await entityStore.CreateAsync(entity);
+
+        var selectionResult = await entityStore.FindByIdAsync(entity.Id);
+
+        selectionResult.State.Should().BeTrue();
+        selectionResult.ErrorCount.Should().Be(0);
+        selectionResult.Payload.Should().NotBeNull().And.BeOfType<BaseEntity>();
+        selectionResult.Payload.As<BaseEntity>().Should().Be(entity);
+        selectionResult.ToString().Should().Contain("Succeeded");
+    }
+
+    /// <summary>
+    /// Checking the <see cref="IEntityStore{TEntity}"/> to indicate a failed operation, if the entity was not updated
+    /// due to an <b>ObjectInvalidFailure</b>.
+    /// </summary>
+    [Test]
+    public async Task FindByIdAsync_FailsWithObjectInvalidFailure()
+    {
+        await using var ctx = new DatabaseContext(_dbContextOptions);
+        IEntityStore<BaseEntity> entityStore = new BaseEntityStore(ctx, _baseEntityValidator);
+
+        var findByIdResult = await entityStore.FindByIdAsync(Guid.Empty);
+
+        findByIdResult.State.Should().BeFalse();
+        findByIdResult.ErrorCount.Should().Be(1);
+        findByIdResult.Payload.Should().NotBeNull().And.BeOfType<Error[]>();
+        findByIdResult.ToString().Should()
+            .ContainAll(nameof(ErrorDescriber.ObjectInvalidFailure));
     }
 }
