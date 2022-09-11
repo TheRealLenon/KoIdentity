e563c72c3a6be10cedc8a6fe3ba674162fd50b9c
TheRealLenon <thereallenon@icloud.com>
2022-09-11 05:50:03 +0200

    Updating tests for the entity store.

--- test/Abstraction.Test/Helper/DatabaseContext.cs
+++ test/Abstraction.Test/Helper/DatabaseContext.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Tekoding.KoIdentity.Abstraction.Extensions;
 
 namespace Tekoding.KoIdentity.Abstraction.Test.Helper;
 
@@ -11,6 +12,14 @@ internal class DatabaseContext : DatabaseContext<BaseEntity>
     {
     }
 
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BaseEntity>().BuildUniqueSqlIndex(e => e.TestProp);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+    
     private sealed class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
     {
         public DatabaseContext CreateDbContext(string[] args)
--- test/Abstraction.Test/Migrations/20220911034814_MakingHelperPropUnique.Designer.cs
+++ test/Abstraction.Test/Migrations/20220911034814_MakingHelperPropUnique.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tekoding.KoIdentity.Abstraction.Test.Helper;
 
@@ -11,9 +12,10 @@ using Tekoding.KoIdentity.Abstraction.Test.Helper;
 namespace Tekoding.KoIdentity.Abstraction.Test.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220911034814_MakingHelperPropUnique")]
+    partial class MakingHelperPropUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
