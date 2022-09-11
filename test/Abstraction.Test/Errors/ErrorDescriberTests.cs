3c068c267a4e2c0371b6dc27afb329ccb136231b
TheRealLenon <thereallenon@icloud.com>
2022-09-11 02:52:18 +0200

    Removing old comments.

--- test/Abstraction.Test/Errors/ErrorDescriberTests.cs
+++ test/Abstraction.Test/Errors/ErrorDescriberTests.cs
@@ -1,17 +1,3 @@
-// KoIdentity Copyright (C) 2022 Tekoding. All Rights Reserved.
-// 
-// Created: 2022.05.29
-// 
-// Authors: TheRealLenon
-// 
-// Licensed under the MIT License. See LICENSE.md in the project root for license
-// information.
-// 
-// KoIdentity is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT
-// License for more details.
-
 using FluentAssertions;
 using NUnit.Framework;
 using Tekoding.KoIdentity.Abstraction.Errors;
