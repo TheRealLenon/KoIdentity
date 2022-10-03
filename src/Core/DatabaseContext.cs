e579f6718c090698f7f6b94123262d23df24ad27
TheRealLenon <thereallenon@icloud.com>
2022-10-04 00:54:05 +0200

    Adjusting database context.

--- src/Core/DatabaseContext.cs
+++ src/Core/DatabaseContext.cs
@@ -8,9 +8,29 @@ namespace Tekoding.KoIdentity.Core;
 
 /// <summary>
 /// Provides the default implementation of the <see cref="Microsoft.EntityFrameworkCore"/> database context used by
-/// KoIdentity.
+/// KoIdentity with the default <see cref="User"/> model.
 /// </summary>
 public class DatabaseContext : DatabaseContext<User>
+{
+    /// <summary>
+    /// Creates a new instance of the <see cref="DatabaseContext"/>.
+    /// </summary>
+    /// <param name="dbContextOptions">
+    /// The <see cref="DbContextOptions"/> being used by a <see cref="DbContext"/>.
+    /// </param>
+    /// <param name="databaseSchema">The database schema name. By Default the schema will be "KoIdentity".</param>
+    public DatabaseContext(DbContextOptions dbContextOptions, string databaseSchema = "KoIdentity") : base(
+        dbContextOptions, databaseSchema)
+    {
+    }
+}
+
+/// <summary>
+/// Provides the default implementation of the <see cref="Microsoft.EntityFrameworkCore"/> database context used by
+/// KoIdentity.
+/// </summary>
+public class DatabaseContext<TUser> : Abstraction.DatabaseContext<TUser>
+    where TUser : User
 {
     /// <summary>
     /// Creates a new instance of the <see cref="DatabaseContext"/>.
@@ -27,7 +47,7 @@ public class DatabaseContext : DatabaseContext<User>
     /// <inheritdoc />
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>().BuildUniqueSqlIndex(u => u.Username);
+        modelBuilder.Entity<TUser>().BuildUniqueSqlIndex(u => u.Username);
 
         modelBuilder.Entity<Role>().BuildDefaultSqlEntity().BuildUniqueSqlIndex(r => r.Name);
 
@@ -36,7 +56,7 @@ public class DatabaseContext : DatabaseContext<User>
             {
                 r.UserId, r.RoleId
             });
-        
+
         base.OnModelCreating(modelBuilder);
     }
 
@@ -48,17 +68,17 @@ public class DatabaseContext : DatabaseContext<User>
     /// <remarks>
     /// The <see cref="User"/> model will be our main entity.
     /// </remarks>
-    public DbSet<User> Users
+    public DbSet<TUser> Users
     {
         get => MainEntities;
         set => MainEntities = value;
     }
-    
+
     /// <summary>
     /// Gets or sets the <see cref="DbSet{TEntity}"/> of roles.
     /// </summary>
     public DbSet<Role> Roles { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the <see cref="DbSet{TEntity}"/> of user roles.
     /// </summary>
